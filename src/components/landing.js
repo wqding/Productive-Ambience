299b791d804bbcc80277855bc777e75bd1efa3f6
Adam Ding <wqding@uwaterloo.ca>
2019-12-15 00:42:12 -0500

    moved states to child

--- src/components/landing.js
+++ src/components/landing.js
@@ -8,11 +8,7 @@ import  tilesConfig from './tilesConfig.js'
 
 import '../styling/landing.css'
 
-
-
 const Landing = (props) => {
-    const [tiles, setTiles] = useState(tilesConfig);
-    
     // const activeStates, volumeStates = [];
     // tiles.forEach( tile => {
     //     activeStates.push({
@@ -21,30 +17,15 @@ const Landing = (props) => {
     //     })
     // })
 
-    const changeVolume = (event, newVolume) => {
-      //change the array somehow depending on the parameters passed to it
-        
 
-    //   setTiles
-    }
-
-    const toggleActive = (name) => {
-        setTiles(
-            tiles.find(tile => {
-                if (tile.name ===name){
-                    tile.active = !tile.active
-                }
-            })
-        );
-    };
     
     const populateGrid = () => {
         let gridLayout = []
-
-        for(let row=0; row < tiles.length/3; row++){
+        let temp = Object.values(tilesConfig)
+        for(let row=0; row < temp.length/3; row++){
             let rowArr = []
             for(let col=0; col < 3; col++){
-                rowArr.push(tiles[row*3 + col])
+                rowArr.push(temp[row*3 + col])
             }
             gridLayout.push(rowArr)
         }
@@ -54,7 +35,6 @@ const Landing = (props) => {
 
     return (
         <div id="landing">
-            {console.log("bruh: " +tiles)}
             <Navbar bg="light" variant="light">
                 <Navbar.Brand href="#home">Productive Ambience</Navbar.Brand>
                 <Nav className="mr-auto justify-content-end">
@@ -91,8 +71,6 @@ const Landing = (props) => {
                                     sound={col.sound} 
                                     active={col.active} 
                                     volume={col.volume} 
-                                    toggleActive={toggleActive}
-                                    changeVolume={changeVolume}
                                 />
                             </Col>
                         ))}
