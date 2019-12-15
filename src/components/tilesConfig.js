48a123c47f51820c80f7c4ab7f786b8c21d2b2c1
Adam Ding <wqding@uwaterloo.ca>
2019-12-15 00:42:29 -0500

    changed format of config

--- src/components/tilesConfig.js
+++ src/components/tilesConfig.js
@@ -1,25 +1,49 @@
-var tilesConfig = [
-    {
+var tilesConfig = {
+    fire: {
         name: "fire",
         active: false,
         sound: "../sounds/fire",
         icon: "fas fa-fire",
         volume: 30
     },
-    {
+    river: {
         name: "river",
         active: false,
         sound: "../sounds/river",
         icon: "fas fa-water",
         volume: 30
     },
-    {
+    train: {
         name: "train",
         active: false,
         sound: "../sounds/train",
         icon: "fas fa-train",
         volume: 30
     }
-]
+}
+
+// var tilesConfig = [
+//     {
+//         name: "fire",
+//         active: false,
+//         sound: "../sounds/fire",
+//         icon: "fas fa-fire",
+//         volume: 30
+//     },
+//     {
+//         name: "river",
+//         active: false,
+//         sound: "../sounds/river",
+//         icon: "fas fa-water",
+//         volume: 30
+//     },
+//     {
+//         name: "train",
+//         active: false,
+//         sound: "../sounds/train",
+//         icon: "fas fa-train",
+//         volume: 30
+//     }
+// ]
 
 export default tilesConfig;
