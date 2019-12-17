3579872c4cdbfeaa91cd1644a95a85002879106f
Adam Ding <wqding@uwaterloo.ca>
2019-12-17 17:52:18 -0500

    added user schema

---
+++ UserSchema.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema(
+    {
+        _id: mongoose.Schema.Types.ObjectId,
+        id: Number,
+        username: String,
+        password: String,
+        //is it possible to have schema within a schema?
+        config: []
+    },
+    {timestamps: true}
+);
+
+module.exports = mongoose.model("UserSchema", UserSchema);
