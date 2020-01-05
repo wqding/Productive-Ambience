c8a0cf529d92d443bcc64f9281a7bae51c12b22f
Adam Ding <wqding@uwaterloo.ca>
2020-01-04 23:35:49 -0500

    changed ip address

--- src/env.js
+++ src/env.js
@@ -1,5 +1,5 @@
 const env = {
-   baseUrl: "http://ec2-3-80-152-194.compute-1.amazonaws.com:5000",
+   baseUrl: "http://34.197.116.174:5000",
    // databaseUrl: "mongodb+srv://user:userPassword@cluster0-3xizq.mongodb.net/test?retryWrites=true&w=majority"
 }
 
